e0190c430e7a92ad17285760cee7be1944f4e792
Benjamin Rasmussen <rasben@users.noreply.github.com>
2026-02-06 21:21:11 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rasben.github.io',
-  base: import.meta.env.PROD ? '/montse-yoga' : '/',
   integrations: [svelte()],
 
   vite: {
